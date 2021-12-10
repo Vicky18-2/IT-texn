476180cdf40f2272ad9c5f6554705a0a08b565d0
Viktoriia <vicky.od.00@gmail.com>
2021-12-10 13:27:12 +0200

    Film web service + client (workable)

--- IT_texn_web_service_pz3/src/main/java/com/ITtexn/pz3/client/film_cl/Film.java
+++ IT_texn_web_service_pz3/src/main/java/com/ITtexn/pz3/client/film_cl/Film.java
@@ -19,10 +19,12 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
- *         &lt;element name="description" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="description12" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
  *         &lt;element name="director" type="{http://www.w3.org/2001/XMLSchema}string" maxOccurs="unbounded" minOccurs="0"/>
  *         &lt;element name="filmDuration" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
  *         &lt;element name="genre" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="id_film" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="language" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
  *         &lt;element name="title" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
  *       &lt;/sequence>
  *     &lt;/restriction>
@@ -34,43 +36,48 @@ import javax.xml.bind.annotation.XmlType;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "film", propOrder = {
-    "description",
+    "description12",
     "director",
     "filmDuration",
     "genre",
+    "idFilm",
+    "language",
     "title"
 })
 public class Film {
 
-    protected String description;
+    protected String description12;
     @XmlElement(nillable = true)
     protected List<String> director;
     protected String filmDuration;
     protected String genre;
+    @XmlElement(name = "id_film")
+    protected int idFilm;
+    protected String language;
     protected String title;
 
     /**
-     * Gets the value of the description property.
+     * Gets the value of the description12 property.
      * 
      * @return
      *     possible object is
      *     {@link String }
      *     
      */
-    public String getDescription() {
-        return description;
+    public String getDescription12() {
+        return description12;
     }
 
     /**
-     * Sets the value of the description property.
+     * Sets the value of the description12 property.
      * 
      * @param value
      *     allowed object is
      *     {@link String }
      *     
      */
-    public void setDescription(String value) {
-        this.description = value;
+    public void setDescription12(String value) {
+        this.description12 = value;
     }
 
     /**
@@ -148,6 +155,46 @@ public class Film {
      */
     public void setGenre(String value) {
         this.genre = value;
+    }
+
+    /**
+     * Gets the value of the idFilm property.
+     * 
+     */
+    public int getIdFilm() {
+        return idFilm;
+    }
+
+    /**
+     * Sets the value of the idFilm property.
+     * 
+     */
+    public void setIdFilm(int value) {
+        this.idFilm = value;
+    }
+
+    /**
+     * Gets the value of the language property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getLanguage() {
+        return language;
+    }
+
+    /**
+     * Sets the value of the language property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setLanguage(String value) {
+        this.language = value;
     }
 
     /**
--- IT_texn_web_service_pz3/src/main/java/com/ITtexn/pz3/client/film_cl/package-info.java
+++ IT_texn_web_service_pz3/src/main/java/com/ITtexn/pz3/client/film_cl/package-info.java
@@ -1,2 +1,2 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://film.service.pz3.ITtexn.com/")
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://film_cl.client.pz3.ITtexn.com/")
 package com.ITtexn.pz3.client.film_cl;
